944a635f91df6be596bc13868a90becae83c0c6f
chrisIyer <hellohotbox420@gmail.com>
2023-10-14 19:44:51 -0400

    changes.

---
+++ src/components/application.tsx
@@ -0,0 +1,14 @@
+// import ControlPanel from './controls';
+
+import NameBadge from './name-badge';
+
+const Application = () => (
+
+
+
+  <main className="application">
+    <NameBadge name={'Chris Iyer'} greeting="Salutations" />
+  </main>
+);
+
+export default Application;
